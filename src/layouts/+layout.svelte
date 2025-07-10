fea76c4d0f0460dac5b7b5f0da9ce295c16bd16a
lisagjh <lisagjhoorn@outlook.com>
2025-07-10 20:13:11 +0200

    WIP redo

---
+++ src/layouts/+layout.svelte
@@ -0,0 +1,46 @@
+<script>
+    import { onNavigate } from "$app/navigation";
+  
+    import Header from "$lib/components/sections/Header.svelte";
+  
+  
+    onNavigate((navigation) => {
+      if (!document.startViewTransition) return;
+  
+      return new Promise((resolve) => {
+        document.startViewTransition(async () => {
+          resolve();
+          await navigation.complete;
+        });
+      });
+    });
+  </script>
+  
+  <div id="skip">
+    <a href="#content">Skip to main content</a>
+  </div>
+  <div class="main-container">
+    <Header />
+    
+    <main id="content">
+      <slot></slot>
+    </main>
+      
+  </div>
+  
+  <style>
+    #skip a {
+      display: block;
+      position: absolute;
+      left: -999px;
+      top: -999px;
+      }
+  
+  #skip a:focus {
+      left: 0;
+      top: 0;
+      padding: .1rem;
+      background: var(--white);
+      outline:1px solid var(--blue);
+      }
+  </style>
