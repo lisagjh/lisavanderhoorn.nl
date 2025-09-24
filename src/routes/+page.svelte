3fdeda246c30e7bf2e652e8949b51d707b672314
Lisa <131701505+lisagjh@users.noreply.github.com>
2025-09-24 17:46:23 +0200

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,9 @@
 	import Layout from '../layouts/+layout.svelte';
 	import Intro from './../lib/components/sections/Intro.svelte';
 	import About from './../lib/components/sections/About.svelte';
-	import Reveal from '$lib/components/sections/Reveal.svelte';
 </script>
 
 <Layout>
-	<!-- <Reveal /> -->
 	<Intro />
 	<About />
 </Layout>
