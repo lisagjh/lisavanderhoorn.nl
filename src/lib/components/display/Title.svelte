a50d5f03e1115e0957dbf1b92db88f45397cba28
lisagjh <lisagjhoorn@outlook.com>
2025-11-12 14:58:13 +0100

    WIP gsap anims on text

---
+++ src/lib/components/display/Title.svelte
@@ -0,0 +1,105 @@
+<script>
+	import gsap from 'gsap';
+	import { ScrollTrigger, SplitText } from 'gsap/all';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		gsap.registerPlugin(ScrollTrigger, SplitText);
+
+		const tl = gsap.timeline();
+
+		const lastName = document.querySelector('.last');
+		const firstName = document.querySelector('.first');
+
+		const subHeading = document.querySelector('.subheading');
+		const subSplit = new SplitText(subHeading, { type: 'chars', mask: 'chars' });
+
+		gsap.to(lastName, {
+			x: '-115%',
+			ease: 'back.inOut(1.7)',
+			duration: 1,
+			scrollTrigger: {
+				trigger: '.wrapper',
+				start: 'top top',
+				end: 'bottom 50%',
+				scrub: true
+			}
+		});
+
+		gsap.to(firstName, {
+			x: '15%',
+			ease: 'back.inOut(1.7)',
+			duration: 1,
+			scrollTrigger: {
+				trigger: '.wrapper',
+				start: 'top top',
+				end: 'bottom 50%',
+				scrub: true
+			}
+		});
+
+        // animate the text out on top and in from bottom, repeat 3 times
+		for (let i = 0; i < 3; i++) {
+			tl.to(
+				subSplit.chars,
+				{
+					y: '-100%',
+					autoAlpha: 0,
+					stagger: 0.03,
+					duration: 0.4,
+					ease: 'power2.inOut'
+				},
+				i > 0 ? '+=0.2' : undefined
+			) // pauze na eerste loop
+				.set(subSplit.chars, {
+					y: '100%'
+				})
+				.to(subSplit.chars, {
+					y: '0%',
+					autoAlpha: 1,
+					duration: 0.4,
+					stagger: 0.03,
+					ease: 'power2.inOut'
+				});
+		}
+	});
+</script>
+
+<div class="wrapper">
+	<h1 class="first">Lisa</h1>
+	<h1 class="last">van der Hoorn</h1>
+	<p class="subheading">Frontend Developer</p>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: 0.5rem;
+		height: 100vh;
+		width: 100vw;
+		overflow: visible;
+	}
+	h1 {
+		font-family: var(--heading-font);
+		font-size: 10rem;
+		text-transform: uppercase;
+		text-wrap: nowrap;
+		letter-spacing: -5px;
+		text-box: trim-both cap alphabetic;
+	}
+
+	h1.first {
+		font-size: 15rem;
+	}
+
+	p {
+		/* font-family: var(--heading-font); */
+		font-weight: bold;
+		font-size: 2rem;
+		color: var(--color-accent);
+		align-self: center;
+		text-transform: lowercase;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,67 +1,19 @@
 <script lang="ts">
-	import gsap from 'gsap';
-	import { SplitText } from 'gsap/all';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		gsap.registerPlugin(SplitText);
-		let heading = SplitText.create('h1', { type: 'words, chars', mask: 'words' });
-		gsap.from(heading.chars, {
-			duration: 0.65,
-			y: -100,
-			autoAlpha: 0,
-			stagger: 0.05
-		});
-
-		let text = SplitText.create('p', { type: 'words', mask: 'words' });
-		gsap.from(text.words, {
-			delay: 0.5,
-			duration: 0.65,
-			y: 100,
-			autoAlpha: 0,
-			stagger: 0.05
-		});
-
-		let span = SplitText.create('span', { type: 'words', mask: 'words' });
-		gsap.from(span.words, {
-			delay: 1.75,
-			duration: 0.75,
-			y: 100,
-			autoAlpha: 0,
-			stagger: 0.05
-		});
-	});
+	import Title from '$lib/components/display/Title.svelte';
 </script>
 
 <section class="one">
-	<h1>Hello!</h1>
-	<p>
-		I'm Lisa, an aspiring frontend developer
-		<br />
-		<span>and web hobbyist!</span>
-	</p>
+	<Title />
 </section>
 
-<section class="two"></section>
-<section class="three"></section>
+<div></div>
+<div></div>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: end;
-		padding: 2.5rem 1.5rem;
-		height: 80vh;
-	}
-
-	h1 {
-		font-weight: 900;
-	}
-
-	p {
-		font-weight: 600;
-		letter-spacing: -0.7px;
-		position: relative;
-		z-index: 10;
+	div {
+		height: 100vh;
+		width: 100vw;
+		background-color: var(--color-background);
+		border: 1px solid red;
 	}
 </style>
