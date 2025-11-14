c70fa3ece3cbc703d1784c07b17438c2ce2df57c
lisagjh <lisagjhoorn@outlook.com>
2025-11-14 21:53:54 +0100

    start new version

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,12 @@ import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
 
+import svelte from "@astrojs/svelte";
+
 export default defineConfig({
   site: "https://lisavanderhoorn.nl/",
   output: "static",
-  integrations: [react(), sitemap()],
+  integrations: [react(), sitemap(), svelte()],
   server: {
     port: 4321, // Set the port for the development server
   },
